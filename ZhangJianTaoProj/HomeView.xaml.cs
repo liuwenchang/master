4c156c01035e0821934e82625a2df1ebbefb4bff
liuwenchang <liuwenchang@thunisoft.com>
2018-02-28 19:11:38 +0800

    新建

---
+++ ZhangJianTaoProj/HomeView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ZhangJianTaoProj
+{
+    /// <summary>
+    /// Interaction logic for AboutView.xaml
+    /// </summary>
+    public partial class HomeView : UserControl
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ZhangJianTaoProj/PrivateView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ZhangJianTaoProj
+{
+    /// <summary>
+    /// Interaction logic for AboutView.xaml
+    /// </summary>
+    public partial class PrivateView : UserControl
+    {
+        public PrivateView()
+        {
+            InitializeComponent();
+        }
+    }
+}
