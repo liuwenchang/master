2aa804518217c47197f7c617f5d60f7e5995ab6d
liuwenchang <2668984518@qq.com>
2020-04-07 21:48:56 +0800

    添加文件

---
+++ MahApps.Metro-develop/readme.md
@@ -0,0 +1,137 @@
+<div align="center">
+  <a href="https://github.com/MahApps/MahApps.Metro">
+    <img alt="MahApps.Metro" width="200" heigth="200" src="https://user-images.githubusercontent.com/658431/30968270-0e3a855e-a45f-11e7-862b-8d92ebd301ad.png">
+  </a>
+  <h1>MahApps.Metro</h1>
+  <p>
+    A toolkit for creating metro-style WPF applications. Lots of goodness out-of-the box.
+  </p>
+  <a href="https://gitter.im/MahApps/MahApps.Metro">
+    <img src="https://img.shields.io/badge/Gitter-Join%20Chat-green.svg?style=flat-square">
+  </a>
+  <a href="https://twitter.com/punker76">
+    <img src="https://img.shields.io/badge/twitter-%40punker76-55acee.svg?style=flat-square">
+  </a>
+  <br />
+  <a href="https://www.nuget.org/packages/MahApps.Metro">
+    <img src="https://img.shields.io/nuget/dt/MahApps.Metro.svg?style=flat-square">
+  </a>
+  <a href="https://www.nuget.org/packages/MahApps.Metro/">
+    <img src="https://img.shields.io/nuget/v/MahApps.Metro.svg?style=flat-square">
+  </a>
+  <a href="https://www.nuget.org/packages/MahApps.Metro/">
+    <img src="https://img.shields.io/nuget/vpre/MahApps.Metro.svg?style=flat-square&label=nuget-pre">
+  </a>
+  <a href="https://github.com/MahApps/MahApps.Metro/releases/latest">
+    <img src="https://img.shields.io/github/release/MahApps/MahApps.Metro.svg?style=flat-square">
+  </a>
+  <br />
+  <a href="https://ci.appveyor.com/project/punker76/mahapps-metro/branch/master">
+    <img alt="masterstatus" src="https://img.shields.io/appveyor/ci/punker76/mahapps-metro/master.svg?style=flat-square&&label=master">
+  </a>
+  <a href="https://ci.appveyor.com/project/punker76/mahapps-metro/branch/develop">
+    <img alt="devstatus" src="https://img.shields.io/appveyor/ci/punker76/mahapps-metro/develop.svg?style=flat-square&&label=develop">
+  </a>
+  <a href="https://ci.appveyor.com/project/punker76/mahapps-metro/branch/develop">
+    <img alt="devtests" src="https://img.shields.io/appveyor/tests/punker76/mahapps-metro/develop.svg?style=flat-square">
+  </a>
+  <br />
+  <a href="https://github.com/MahApps/MahApps.Metro/issues">
+    <img src="https://img.shields.io/github/issues-raw/MahApps/MahApps.Metro.svg?style=flat-square">
+  </a>
+  <a href="https://github.com/MahApps/MahApps.Metro/issues">
+    <img src="https://img.shields.io/github/issues-closed-raw/MahApps/MahApps.Metro.svg?style=flat-square">
+  </a>
+  <a href="https://github.com/MahApps/MahApps.Metro/issues">
+    <img src="https://img.shields.io/github/issues-pr-raw/MahApps/MahApps.Metro.svg?style=flat-square">
+  </a>
+  <a href="https://github.com/MahApps/MahApps.Metro/issues">
+    <img src="https://img.shields.io/github/issues-pr-closed-raw/MahApps/MahApps.Metro.svg?style=flat-square">
+  </a>
+</div>
+
+<h2>Want to say thanks?</h2>
+
+This framework is free and can be used for free, open source and commercial applications. It's tested and contributed by many people... So mainly hit the :star: button, that's all... thx :squirrel: (:dollar:, :euro:, :beer: or some other gifts are also being accepted...).
+
+<h2>Let's get started</h2>
+
+- [Documentation](https://github.com/MahApps/MahApps.Metro/wiki/Documentation)
+- [Releases and Release Notes](https://github.com/MahApps/MahApps.Metro/releases)
+- [Quick Start](https://github.com/MahApps/MahApps.Metro/wiki/Quick-Start)
+- [Icons](https://github.com/MahApps/MahApps.Metro/wiki/Icons) (MahApps.Metro.IconPacks)
+- [Contributing](https://github.com/MahApps/MahApps.Metro/wiki/Contributing) to MahApps.Metro
+- [Building](https://github.com/MahApps/MahApps.Metro/wiki/Building-the-MahApps.Metro-solution) the MahApps.Metro solution
+- [Visual Studio Templates](https://github.com/MahApps/MahApps.Metro/wiki/Visual-Studio-Templates)
+- [Wiki](https://github.com/MahApps/MahApps.Metro/wiki)
+
+<h2>Something missing?</h2>
+
+If you're looking for another awesome libraries which are compatible with MahApps, check them out:
+
+- [MaterialDesignInXamlToolkit](https://github.com/ButchersBoy/MaterialDesignInXamlToolkit) Google Material Design in XAML & WPF, for C# & VB.Net [http://materialdesigninxaml.net](http://materialdesigninxaml.net)
+- [MahApps.Metro.SimpleChildWindow](https://github.com/punker76/MahApps.Metro.SimpleChildWindow) A simple child window for MahApps.Metro
+- [Loading indicators](https://github.com/100GPing100/LoadingIndicators.WPF) by [@100GPing100](https://github.com/100GPing100)
+- [Dragablz](https://github.com/ButchersBoy/Dragablz) Tearable tab control for WPF, which includes docking, tool windows and MDI. [http://dragablz.net](http://dragablz.net)
+
+<h2>Tools</h2>
+
+We use these awesome tools to build and develop MahApps.Metro.
+
+<div>
+  <a href="https://cakebuild.net/">
+    <img alt="cakebuild.net" width="128" heigth="128" src="./docs/cake-medium.png">
+  </a>
+  <a href="https://www.jetbrains.com/resharper/">
+    <img alt="R#" width="128" heigth="128" hspace="40" src="./docs/icon_ReSharper.png">
+  </a>
+  <a href="https://www.appveyor.com/">
+    <img alt="appveyor" width="128" heigth="128" src="./docs/Appveyor_logo.svg">
+  </a>
+</div>
+
+<h2>Screenshots</h2>
+
+<div align="center">
+
+<img alt="screenshot01" src="./docs/2018-02-15_22h54_57.png">
+
+<img alt="screenshot02" src="./docs/2018-02-15_22h55_19.png">
+
+<img alt="screenshot03" src="./docs/2018-02-15_22h55_52.png">
+
+<img alt="screenshot04" src="./docs/2018-02-15_22h50_39.png">
+
+<img alt="screenshot05" src="./docs/2018-02-15_22h51_03.png">
+
+<img alt="screenshot06" src="./docs/2018-02-15_22h51_22.png">
+
+<img alt="screenshot07" src="./docs/2018-02-15_22h52_01.png">
+
+<img alt="screenshot08" src="./docs/2018-02-15_22h52_26.png">
+
+<img alt="screenshot09" src="./docs/2018-02-15_22h53_14.png">
+
+<img alt="screenshot10" src="./docs/2018-02-15_22h53_41.png">
+
+<img alt="screenshot11" src="./docs/2018-02-15_22h56_33.png">
+
+<img alt="screenshot12" src="./docs/2018-02-15_22h57_16.png">
+
+<img alt="screenshot13" src="./docs/2018-02-15_22h57_37.png">
+
+<img alt="screenshot14" src="./docs/2018-02-15_22h57_51.png">
+
+<img alt="screenshot15" src="./docs/2018-02-15_23h00_35.png">
+
+<img alt="screenshot16" src="./docs/main_demo_flyout1.png">
+
+<img alt="screenshot17" src="./docs/main_demo_flyout2.png">
+
+<img alt="screenshot18" src="./docs/mahapps_v1.6.0.gif">
+
+</div>
+
+<h2>Licence</h2>
+
+[MIT License (MIT)](./LICENSE)
---
+++ ZhangJianTaoProj/InputData.xaml.cs
@@ -0,0 +1,178 @@
+﻿using MahApps.Metro.Controls;
+using MathWorks.MATLAB.NET.Arrays;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ZhangJianTaoProj.Model;
+using ZhangJianTaoProj.Provider;
+using zjtMatlab;
+
+namespace ZhangJianTaoProj
+{
+    /// <summary>
+    /// InputData.xaml 的交互逻辑
+    /// </summary>
+    public partial class InputData : UserControl
+    {
+
+        public delegate void CalcEnd(bool isHadData);
+        public event CalcEnd CalcEnd_Handle;
+        public InputData()
+        {
+            InitializeComponent();
+        }
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            bool? result = dialog.ShowDialog();
+            if (result == true)
+            {
+                if (sender == OpenFileA)
+                {
+                    this.SelectedFileA.Text = dialog.FileName;
+                    CalcParamProvider.totalModels.Insert(0,new CalcParam(dialog.FileName, 1));
+                }
+                if (sender == OpenFileB)
+                {
+                    this.SelectedFileB.Text = dialog.FileName;
+                    CalcParamProvider.totalModels.Insert(1, new CalcParam(dialog.FileName, 2));
+                }
+                if (sender == OpenFileC)
+                {
+                    CalcParamProvider.totalModels.Insert(2, new CalcParam(dialog.FileName, 3));
+                    this.SelectedFileC.Text = dialog.FileName;
+                }
+            }
+        }
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (CalcParamProvider.isHadCeXiangData)
+            {
+                setResImgSource();
+                this.SelectedFileA.Text = CalcParamProvider.totalModels[0].DataPath;
+                this.SelectedFileB.Text = CalcParamProvider.totalModels[1].DataPath;
+                this.SelectedFileC.Text = CalcParamProvider.totalModels[2].DataPath;
+
+            }
+            else
+            {
+                setResImgNull();
+            }
+
+        }
+        private bool CheckModelIsInit()
+        {
+            if (CalcParamProvider.totalModels.Count < CalcParamProvider.totalModelsMinCount)
+                return false;
+            foreach (var item in CalcParamProvider.totalModels)
+            {
+                if (item == null)
+                    return false;
+            }
+            return true;
+        }
+        private async Task<bool> calc(CalcParam model)
+        {
+            return await Task.Run(() =>
+            {
+                try
+                {
+                    int ceXiangResCount = 4;
+                    using (NoiseReduce re = new NoiseReduce())
+                    {
+                        re.InputData(model.DataPath, model.Index);
+                        re.DoubleDataExp(model.DataPath, model.Index);
+                        MWArray[] angle = re.Direct(ceXiangResCount, model.DataPath, model.Index);
+                        for (int i = 0; i < ceXiangResCount; i++)
+                        {
+                            double t = Convert.ToDouble(angle[i].ToString());
+                            model.ceXiangRes.Add(t);
+                        }
+                        return true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Logger.Default.Error("数据导入计算错误", ex);
+                    return false;
+                }
+            });
+        }
+        private void clearOldData()
+        {
+            CalcParamProvider.isHadCeXiangData = false;
+            CalcParamProvider.isHadLocationData = false;
+            CalcParamProvider.CeXiangResList.Clear();
+            CalcParamProvider.LocationResList.Clear();
+        }
+        private async void Start_Click(object sender, RoutedEventArgs e)
+        {
+             
+            if (!CheckModelIsInit())
+            {
+                MessageBox.Show("请分别选择"+ CalcParamProvider.totalModelsMinCount + "组数据");
+                return;
+            }
+            clearOldData();
+            Start.Content = "正在处理数据，大约需要3分钟";
+            Start.IsEnabled = false;
+            setResImgNull();
+            StartProgress.Visibility = Visibility.Visible;
+            List<Task<bool>> tasks = new List<Task<bool>>();
+            //tasks.Add(calc(totalModels[0]));
+            //await Task.Run(() =>
+            //{
+            //    ParallelLoopResult res = Parallel.ForEach<Model>(totalModels, str =>
+            //    {
+            //        calc(str); //效率跟task差不多
+            //    });
+            //});
+            foreach (var item in CalcParamProvider.totalModels)
+            {
+                tasks.Add(calc(item));
+            }
+            foreach (var item in tasks)
+            {
+               bool res = await item;
+                if (!res)
+                {
+                    MessageBox.Show("计算出错");
+                    Start.IsEnabled = true;
+                    return;
+                }
+            }
+            //angle.ArrayType
+            //angle.ToArray().ge
+            setResImgSource();
+            CalcParamProvider.saveCeXiangData();
+            Start.Content = "开始计算";
+            Start.IsEnabled = true;
+            StartProgress.Visibility = Visibility.Hidden;
+            CalcParamProvider.isHadCeXiangData = true;
+            CalcEnd_Handle?.Invoke(CalcParamProvider.isHadCeXiangData);
+        }
+        private void setResImgNull()
+        {
+            ShowPic.clearPic();
+        }
+        private void setResImgSource()
+        {
+            ShowPic.imgA1 = CalcParamProvider.getMemroyImg(CalcParamProvider.totalModels[0].getImgPath(CalcParam.ModelType.Input)[0]);
+            ShowPic.imgB1 = CalcParamProvider.getMemroyImg(CalcParamProvider.totalModels[1].getImgPath(CalcParam.ModelType.Input)[0]);
+            ShowPic.imgC1 = CalcParamProvider.getMemroyImg(CalcParamProvider.totalModels[2].getImgPath(CalcParam.ModelType.Input)[0]);
+        }
+    }
+}
